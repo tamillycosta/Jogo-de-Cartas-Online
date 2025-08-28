1f3e062309e2c4f336ee6a0f6fbdaa99f3b704cb
Tamilly costa <Tamillycosta899@gmail.com>
2025-08-28 10:41:36 -0300

    config: Configura Db

---
+++ server/game/config/config.go
@@ -0,0 +1,41 @@
+package config
+
+
+import (
+    "fmt"
+    "os"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"jogodecartasonline/server/game/models"
+)
+
+
+
+var user = os.Getenv("DB_USER")
+var password = os.Getenv("DB_PASSWORD")
+var dbname = os.Getenv("DB_NAME")
+var host = os.Getenv("DB_HOST")
+var port = os.Getenv("DB_PORT")
+
+var dsn = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+    user, password, host, port, dbname)
+
+
+
+func CretaeTable() gorm.DB{
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Falha ao conectar no banco de dados")
+	}
+
+	// Cria as tabelas automaticamente
+	err = db.AutoMigrate(&models.Player{}, &models.Card{})
+	if err != nil {
+		panic("Falha ao criar tabelas")
+	}
+	fmt.Print("mIGRAÇÃO FEITA")
+	return  *db
+}
+
+
+
