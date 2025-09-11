ac7ee8dab445d8cfb12a50288eb4fa806c35a5d4
Tamilly costa <Tamillycosta899@gmail.com>
2025-09-11 12:37:57 -0300

    refactor: refatora tratamento de responses das partidas e abertura de pacotes

---
+++ client/systemState.go
@@ -0,0 +1,62 @@
+package main
+
+import(	"sync")
+
+// Status do sistema de pacotes 
+var (
+	PackageMenuActive bool
+	WaitingForPackage bool // NOVO: Indica se está aguardando resposta do servidor
+	packageMutex      sync.RWMutex
+)
+
+func SetPackageMenuActive(active bool) {
+	packageMutex.Lock()
+	defer packageMutex.Unlock()
+	PackageMenuActive = active
+}
+
+func IsPackageMenuActive() bool {
+	packageMutex.RLock()
+	defer packageMutex.RUnlock()
+	return PackageMenuActive
+}
+
+func SetWaitingForPackage(waiting bool) {
+	packageMutex.Lock()
+	defer packageMutex.Unlock()
+	WaitingForPackage = waiting
+}
+
+func IsWaitingForPackage() bool {
+	packageMutex.RLock()
+	defer packageMutex.RUnlock()
+	return WaitingForPackage
+}
+
+
+// Status da partida 
+type MatchmakingState struct {
+	IsSearching bool
+	InGame      bool
+	CurrentTurn string
+	GameState   map[string]interface{}
+	mu          sync.RWMutex
+}
+
+func (ms *MatchmakingState) SetSearching(searching bool) {
+	ms.mu.Lock()
+	defer ms.mu.Unlock()
+	ms.IsSearching = searching
+}
+
+func (ms *MatchmakingState) SetInGame(inGame bool) {
+	ms.mu.Lock()
+	defer ms.mu.Unlock()
+	ms.InGame = inGame
+}
+
+func (ms *MatchmakingState) IsInAnyState() bool {
+	ms.mu.RLock()
+	defer ms.mu.RUnlock()
+	return ms.IsSearching || ms.InGame || PackageMenuActive
+}
